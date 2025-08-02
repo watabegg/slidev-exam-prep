c190317dae7e57637bf9f5904de403f0f56aa0a4
watabegg <watabegg@gmail.com>
2025-08-03 08:50:21 +0900

    hai

--- layouts/cover.vue
+++ layouts/cover.vue
@@ -7,6 +7,17 @@
     <div v-if="$frontmatter.author" class="author">
       {{ $frontmatter.author }}
     </div>
+    <div class="wave">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1280 700" width="100%" height="100%" preserveAspectRatio="none">
+        <defs>
+          <linearGradient id="wave-1754178473929" gradientUnits="userSpaceOnUse" x1="0" y1="0" x2="700" y2="700" gradientTransform="scale(1.83, 1)">
+            <stop offset="0%" stop-color="#a5d6a7"/>
+            <stop offset="100%" stop-color="#2e7d32"/>
+          </linearGradient>
+        </defs>
+        <path d="M -1049.6 400 Q -729.6 400 -409.6 550 Q 230.4 850 870.4 550 Q 1190.4 400 1510.4 400 Q 1830.4 400 2150.4 550 Q 2790.4 850 3430.4 550 Q 3750.4 400 4070.4 400 L 1280 0 L 0 0 Z" fill="url(#wave-1754178473929)"></path>
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -23,5 +34,14 @@
 .default {
   margin-top: 2rem;
   color: white;
+}
+
+.wave {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
 }
 </style>
